05bcafe1be085d6fd261f07197a38b7c04ebec75
Dmitriy Shubenok <dshubenok93@gmail.com>
2022-08-18 16:49:02 +0600

    Stage 5/18: Get and count active passcards using .filter

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     all_passcards = Passcard.objects.all()
-    active_passcards = []
-    for passcard in all_passcards:
-        if passcard.is_active is True:
-            active_passcards.append(passcard)
+    active_passcards = Passcard.objects.filter(is_active=True)
+
     print(f'''
     Всего пропусков: {len(all_passcards)}
     Активных пропусков: {len(active_passcards)}
